53f205612a708dad8dba97d4e6156791b4f942d0
jscamposx <jscamposx@gmail.com>
2025-11-01 17:27:34 -0600

    agregando seo y curriculum

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jscamposx.com', // Actualiza con tu URL real
   // @ts-expect-error: View transitions flag is available in Astro 5.x but missing in the shipped d.ts
   viewTransitions: true,
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
