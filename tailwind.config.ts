8e65026499b230ab5c806cbb78c3cd498e666018
jscamposx <jscamposx@gmail.com>
2025-11-01 08:36:01 -0600

    test

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+} satisfies Config;
